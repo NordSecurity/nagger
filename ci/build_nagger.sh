7e49c602afa383cd350cef53398115b2cf6520b5
Tomasz Kłak <tomasz.klak@nordsec.com>
2023-10-30 11:33:31 +0100

    Add nagger docker build/push job
    Signed-off-by: Tomasz Kłak <tomasz.klak@nordsec.com>

--- ci/build_nagger.sh
+++ ci/build_nagger.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -euox
 
-source $(dirname $0)/env.sh
+BASE_DIR=$PWD
 
-if [[ ! -d "${BASE_DIR}/dist" ]]; then
-    mkdir "${BASE_DIR}/dist"
-fi
+mkdir -p "${BASE_DIR}/dist"
 
 TARGET="x86_64-unknown-linux-gnu"
 
 pushd "${BASE_DIR}/existing_lints"
 cargo build --target ${TARGET} --release
+
+RUST_CHANNEL=nightly-2023-07-02
+UNWRAP_CHECK_LINT_BIN=libunwrap_check@${RUST_CHANNEL}-x86_64-unknown-linux-gnu.so
+MPSC_BLOCKING_SEND_LINT_BIN=libmpsc_blocking_send@${RUST_CHANNEL}-x86_64-unknown-linux-gnu.so
+LARGE_FUTURES_LINT_BIN=liblarge_futures@${RUST_CHANNEL}-x86_64-unknown-linux-gnu.so
+INDEX_ACCESS_LINT_BIN=libindex_access_check@${RUST_CHANNEL}-x86_64-unknown-linux-gnu.so
 
 cp "${BASE_DIR}/existing_lints/target/${TARGET}/release/${UNWRAP_CHECK_LINT_BIN}" "${BASE_DIR}/dist/"
 cp "${BASE_DIR}/existing_lints/target/${TARGET}/release/${MPSC_BLOCKING_SEND_LINT_BIN}" "${BASE_DIR}/dist/"
